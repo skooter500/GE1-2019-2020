e037c0dcaba3493c7f82128bbf8f55c589c8f6ec
skooter500 <skooter500@gmail.com>
2019-10-11 15:59:46 +0100

    bla

--- GE1Examples2019/Assets/AITank.cs
+++ GE1Examples2019/Assets/AITank.cs
@@ -6,17 +6,18 @@ public class AITank : MonoBehaviour
 {
     public int numWaypoints = 5;
     public float radius = 10;
-    List<Vector3> wayPoints;
+    List<Vector3> wayPoints = new List<Vector3>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        CalculateWaypoints();
     }
 
-    public void OnDrawGizmos()
+    void CalculateWaypoints()
     {
-        float gap = (Mathf.PI * 2.0f) / (float) numWaypoints;
-        for(int i = 0; i < numWaypoints; i ++)
+        wayPoints.Clear();
+        float gap = (Mathf.PI * 2.0f) / (float)numWaypoints;
+        for (int i = 0; i < numWaypoints; i++)
         {
             Vector3 pos = new Vector3(
                 Mathf.Sin(gap * i) * radius
@@ -24,13 +25,32 @@ public class AITank : MonoBehaviour
                 , Mathf.Cos(gap * i) * radius
                 );
             pos = transform.TransformPoint(pos);
-            Gizmos.DrawWireSphere(pos, 2);
+            wayPoints.Add(pos);
         }
     }
+
+    public void OnDrawGizmos()
+    {
+        CalculateWaypoints();
+        foreach(Vector3 p in wayPoints)
+        {
+            Gizmos.DrawWireSphere(p, 2);
+        }            
+    }
+
+    float speed = 5.0f;
+    int current = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.position = Vector3.MoveTowards()
+        if (Vector3.Distance(transform.position, wayPoints[current]) < 1.0f)
+        {
+            current = (current + 1) % wayPoints.Count;
+        }
+        transform.LookAt(wayPoints[current]);
+        transform.Translate(0, 0, speed * Time.deltaTime);
+
     }
 }
--- GE1Examples2019/Assets/Shooting.cs
+++ GE1Examples2019/Assets/Shooting.cs
@@ -7,17 +7,51 @@ public class Shooting : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        
+        //StartCoroutine(ShootingCoroutine());
     }
+
+    public int fireRate = 2;
+    float ellapsed = float.MaxValue;
+    
+
+    System.Collections.IEnumerator ShootingCoroutine()
+    {
+        float toPass = 1.0f / fireRate;
+        while (true)
+        {
+            Debug.Log("Shooting!");
+            GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            yield return new WaitForSeconds(toPass);
+
+        }
+    }
+
+    Coroutine cr;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            cr = StartCoroutine(ShootingCoroutine());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(cr);
+        }
+        
+        // Without coroutines
+        /*float toPass = 1.0f / fireRate;
+        ellapsed += Time.deltaTime;
+        if (Input.GetAxis("Fire1") == 1 && ellapsed > toPass)
         {
             GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            ellapsed = 0;
         }
+        */
+        
     }
 }
