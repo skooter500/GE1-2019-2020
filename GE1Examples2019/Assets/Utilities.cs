15819df98cfbd6cac4dfa3000652cac28659fc36
Bryan Duggan <skooter500@gmail.com>
2019-11-08 12:34:39 +0000

    bla

---
+++ GE1Examples2019/Assets/Utilities.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Utilities
+{
+    public static Color RandomColor()
+    {
+        return Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+    }
+
+}
