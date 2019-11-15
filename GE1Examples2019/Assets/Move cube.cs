21845859c225a40631ce05095c97622f8fdf8ea2
pHoran17 <c16416742@mydit.ie>
2019-11-15 16:50:56 +0000

    Update

---
+++ GE1Examples2019/Assets/Move cube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movecube : MonoBehaviour
+{
+    //Script moves one cube towards a target cube
+    public Transform target;
+    public float time = 5;
+    public float speed = 5;
+    public float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 toTarget = target.position - transform.position;
+        float distance = toTarget.magnitude;
+        speed = distance / time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toTarget = target.position - transform.position;
+        toTarget.Normalize();
+        //distance = toTarget.magnitude;
+        t += Time.deltaTime;
+        transform.position += toTarget * speed * t;
+    }
+}
